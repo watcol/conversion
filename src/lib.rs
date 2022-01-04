b61b4988bf1ca6d16f5dda928320e219d77ab905
watcol <potfman@gmail.com>
2022-01-04 18:54:50 +0900

    Fixed attribution.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 //! An abstraction crate to convert iterators on the fly.
-#![cfg_attr(feature = "std", no_std)]
+#![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "nightly", feature(doc_cfg))]
 
 #[cfg(feature = "alloc")]
