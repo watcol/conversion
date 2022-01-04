ba4048812de6bf29a7273c5bf73e257bcce9297a
watcol <potfman@gmail.com>
2022-01-04 19:27:59 +0900

    Added supporting method on `Converter`.

--- src/lib.rs
+++ src/lib.rs
@@ -16,10 +16,38 @@ pub trait Converter {
     /// The type of errors while conversion.
     type Error;
 
-    /// Consuming one item, stores outputs into `buf` implements [`Extend`], and returns
+    /// Consumes one item, stores outputs into `buf` implements [`Extend`], and returns
     /// the number of stored outputs or a conversion error. If there are no outputs yet,
     /// you should store the item and return `Ok(0)`.
     ///
     /// [`Extend`]: core::iter::Extend
     fn convert<E>(&mut self, item: Self::Item, buf: &mut E) -> Result<usize, Self::Error>;
+
+    /// Returns whether the converter reached the end, or not.
+    ///
+    /// The default return value is `false`, and turning it `true` means the converter will no
+    /// longer produce any outputs. (In other words, the [`convert`] method returns `Ok(0)`
+    /// forever.)
+    ///
+    /// [`convert`]: Self::convert
+    #[inline]
+    fn is_ended(&self) -> bool {
+        false
+    }
+
+    /// Returnd the estimated bounds about the numbers of outputs that one item will produce.
+    ///
+    /// The first element is the lower bound, and the second element is the upper bound. (if
+    /// nothing, you can specify [`None`].)
+    ///
+    /// The [`convert`] method should not return numbers out of this bounds, which may cause
+    /// unexpected errors. The default implementation returns `(0, None)` matches on any
+    /// converters.
+    ///
+    /// [`None`]: core::option::Option::None
+    /// [`convert`]: Self::convert
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, None)
+    }
 }
