c1cd16605a7542f1b7e128ba38b916686a83faa5
watcol <potfman@gmail.com>
2022-01-04 18:52:50 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Conversion
+An abstraction crate to convert iterators on the fly.
+
+## License
+This program is licensed under the MIT license.
+See [LICENSE](https://github.com/watcol/conversion/blob/main/LICENSE) for details.
---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+//! An abstraction crate to convert iterators on the fly.
+#![cfg_attr(feature = "std", no_std)]
+#![cfg_attr(feature = "nightly", feature(doc_cfg))]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
+/// Types to convert N items into M outputs.
+pub trait Converter {
+    /// The type of input items.
+    type Item;
+
+    /// The type of outputs.
+    type Output;
+
+    /// The type of errors while conversion.
+    type Error;
+
+    /// Consuming one item, stores outputs into `buf` implements [`Extend`], and returns
+    /// the number of stored outputs or a conversion error. If there are no outputs yet,
+    /// you should store the item and return `Ok(0)`.
+    ///
+    /// [`Extend`]: core::iter::Extend
+    fn convert<E>(&mut self, item: Self::Item, buf: &mut E) -> Result<usize, Self::Error>;
+}
