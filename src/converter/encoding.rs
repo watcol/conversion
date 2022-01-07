1548c408c7d7fa3beeffbf1ed0222868c3d83c78
watcol <potfman@gmail.com>
2022-01-07 22:53:21 +0900

    Add UTF16 Decoder/Encoder.

--- src/converter/encoding.rs
+++ src/converter/encoding.rs
@@ -1,5 +1,6 @@
 //! ASCII/UTF-8/UTF-16/UTF-32 encoders and decoders as an converter.
 
 pub mod ascii;
+pub mod utf16;
 pub mod utf32;
 pub mod utf8;
--- src/converter/encoding/utf16.rs
+++ src/converter/encoding/utf16.rs
@@ -1,26 +1,30 @@
-//! UTF-32 Decoder/Encoder.
+//! UTF-16 Decoder/Encoder.
 
 use crate::Converter;
 use core::{convert::Infallible, fmt};
 
+use super::utf32::UTF32Decoder;
+
 /// An error while encoding/decoding UTF-32 characters.
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct UTF32EncodingError;
+pub struct UTF16EncodingError;
 
-impl fmt::Display for UTF32EncodingError {
+impl fmt::Display for UTF16EncodingError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "a codepoint is out of bound.")
+        write!(f, "a character is not valid.")
     }
 }
 
 #[cfg(feature = "std")]
-impl std::error::Error for UTF32EncodingError {}
+impl std::error::Error for UTF16EncodingError {}
 
-/// A 32-bit decoder for UTF-32.
+/// A 16-bit decoder for UTF-16.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
-pub struct UTF32Decoder;
+pub struct UTF16Decoder {
+    buf: Option<u16>,
+}
 
-impl UTF32Decoder {
+impl UTF16Decoder {
     /// Create a new instance.
     #[inline]
     pub fn new() -> Self {
@@ -28,90 +32,31 @@ impl UTF32Decoder {
     }
 }
 
-impl Converter for UTF32Decoder {
-    type Item = u32;
+impl Converter for UTF16Decoder {
+    type Item = u16;
     type Output = char;
-    type Error = UTF32EncodingError;
+    type Error = UTF16EncodingError;
 
     fn convert<E>(&mut self, item: Self::Item, buf: &mut E) -> Result<usize, Self::Error>
     where
         E: Extend<Self::Output>,
     {
-        match char::from_u32(item) {
-            Some(c) => {
-                buf.extend([c]);
-                Ok(1)
+        match self.buf {
+            Some(w) if item & 0xFC00 == 0xDC00 => {
+                self.buf = None;
+                UTF32Decoder
+                    .convert(((w & 0x3FF) as u32) << 10 | (item & 0x3FF) as u32, buf)
+                    .map_err(|_| UTF16EncodingError)
             }
-            None => Err(UTF32EncodingError),
-        }
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (1, Some(1))
-    }
-}
-
-/// A 32-bit encoder for UTF-32.
-#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
-pub struct UTF32Encoder;
-
-impl UTF32Encoder {
-    /// Create a new instance.
-    #[inline]
-    pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-impl Converter for UTF32Encoder {
-    type Item = char;
-    type Output = u32;
-    type Error = Infallible;
-
-    fn convert<E>(&mut self, item: Self::Item, buf: &mut E) -> Result<usize, Self::Error>
-    where
-        E: Extend<Self::Output>,
-    {
-        buf.extend([item as u32]);
-        Ok(1)
-    }
-
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        (1, Some(1))
-    }
-}
-
-/// A byte decoder for UTF-32 (big-endian).
-#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
-pub struct UTF32BEDecoder {
-    bytes: [u8; 4],
-    count: usize,
-}
-
-impl UTF32BEDecoder {
-    /// Create a new instance.
-    #[inline]
-    pub fn new() -> Self {
-        Self::default()
-    }
-}
-
-impl Converter for UTF32BEDecoder {
-    type Item = u8;
-    type Output = char;
-    type Error = UTF32EncodingError;
-
-    fn convert<E>(&mut self, item: Self::Item, buf: &mut E) -> Result<usize, Self::Error>
-    where
-        E: Extend<Self::Output>,
-    {
-        self.bytes[self.count] = item;
-        if self.count == 3 {
-            self.count = 0;
-            UTF32Decoder.convert(u32::from_be_bytes(self.bytes), buf)
-        } else {
-            self.count += 1;
-            Ok(0)
+            Some(_) => Err(UTF16EncodingError),
+            None if item & 0xFC00 == 0xD800 => {
+                self.buf = Some(item);
+                Ok(0)
+            }
+            None if item & 0xFC00 == 0xDC00 => Err(UTF16EncodingError),
+            None => UTF32Decoder
+                .convert(item as u32, buf)
+                .map_err(|_| UTF16EncodingError),
         }
     }
 
@@ -120,11 +65,11 @@ impl Converter for UTF32BEDecoder {
     }
 }
 
-/// A byte encoder for UTF-32 (big-endian).
+/// A 16-bit encoder for UTF-16.
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
-pub struct UTF32BEEncoder;
+pub struct UTF16Encoder;
 
-impl UTF32BEEncoder {
+impl UTF16Encoder {
     /// Create a new instance.
     #[inline]
     pub fn new() -> Self {
@@ -132,32 +77,33 @@ impl UTF32BEEncoder {
     }
 }
 
-impl Converter for UTF32BEEncoder {
+impl Converter for UTF16Encoder {
     type Item = char;
-    type Output = u8;
+    type Output = u16;
     type Error = Infallible;
 
     fn convert<E>(&mut self, item: Self::Item, buf: &mut E) -> Result<usize, Self::Error>
     where
         E: Extend<Self::Output>,
     {
-        buf.extend((item as u32).to_be_bytes());
-        Ok(4)
+        let mut tmp_buf = [0u16; 2];
+        let res = item.encode_utf16(&mut tmp_buf).len();
+        buf.extend(tmp_buf);
+        Ok(res)
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        (4, Some(4))
+        (1, Some(2))
     }
 }
 
-/// A byte decoder for UTF-32 (little-endian).
+/// A byte decoder for UTF-16 (big-endian).
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
-pub struct UTF32LEDecoder {
-    bytes: [u8; 4],
-    count: usize,
+pub struct UTF16BEDecoder {
+    byte: Option<u8>,
 }
 
-impl UTF32LEDecoder {
+impl UTF16BEDecoder {
     /// Create a new instance.
     #[inline]
     pub fn new() -> Self {
@@ -165,22 +111,18 @@ impl UTF32LEDecoder {
     }
 }
 
-impl Converter for UTF32LEDecoder {
+impl Converter for UTF16BEDecoder {
     type Item = u8;
     type Output = char;
-    type Error = UTF32EncodingError;
+    type Error = UTF16EncodingError;
 
     fn convert<E>(&mut self, item: Self::Item, buf: &mut E) -> Result<usize, Self::Error>
     where
         E: Extend<Self::Output>,
     {
-        self.bytes[self.count] = item;
-        if self.count == 3 {
-            self.count = 0;
-            UTF32Decoder.convert(u32::from_le_bytes(self.bytes), buf)
-        } else {
-            self.count += 1;
-            Ok(0)
+        match self.byte {
+            Some(b) => UTF16Decoder::new().convert(u16::from_be_bytes([b, item]), buf),
+            None => Ok(0),
         }
     }
 
@@ -189,11 +131,11 @@ impl Converter for UTF32LEDecoder {
     }
 }
 
-/// A byte encoder for UTF-32 (little-endian).
+/// A byte encoder for UTF-16 (big-endian).
 #[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
-pub struct UTF32LEEncoder;
+pub struct UTF16BEEncoder;
 
-impl UTF32LEEncoder {
+impl UTF16BEEncoder {
     /// Create a new instance.
     #[inline]
     pub fn new() -> Self {
@@ -201,7 +143,7 @@ impl UTF32LEEncoder {
     }
 }
 
-impl Converter for UTF32LEEncoder {
+impl Converter for UTF16BEEncoder {
     type Item = char;
     type Output = u8;
     type Error = Infallible;
@@ -210,11 +152,83 @@ impl Converter for UTF32LEEncoder {
     where
         E: Extend<Self::Output>,
     {
-        buf.extend((item as u32).to_le_bytes());
-        Ok(4)
+        let mut tmp_buf = [0u16; 2];
+        let res = item.encode_utf16(&mut tmp_buf).len();
+        for w in tmp_buf {
+            buf.extend(w.to_be_bytes());
+        }
+        Ok(res * 2)
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        (4, Some(4))
+        (2, Some(4))
+    }
+}
+
+/// A byte decoder for UTF-16 (little-endian).
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub struct UTF16LEDecoder {
+    byte: Option<u8>,
+}
+
+impl UTF16LEDecoder {
+    /// Create a new instance.
+    #[inline]
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Converter for UTF16LEDecoder {
+    type Item = u8;
+    type Output = char;
+    type Error = UTF16EncodingError;
+
+    fn convert<E>(&mut self, item: Self::Item, buf: &mut E) -> Result<usize, Self::Error>
+    where
+        E: Extend<Self::Output>,
+    {
+        match self.byte {
+            Some(b) => UTF16Decoder::new().convert(u16::from_le_bytes([b, item]), buf),
+            None => Ok(0),
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(1))
+    }
+}
+
+/// A byte encoder for UTF-16 (little-endian).
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
+pub struct UTF16LEEncoder;
+
+impl UTF16LEEncoder {
+    /// Create a new instance.
+    #[inline]
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl Converter for UTF16LEEncoder {
+    type Item = char;
+    type Output = u8;
+    type Error = Infallible;
+
+    fn convert<E>(&mut self, item: Self::Item, buf: &mut E) -> Result<usize, Self::Error>
+    where
+        E: Extend<Self::Output>,
+    {
+        let mut tmp_buf = [0u16; 2];
+        let res = item.encode_utf16(&mut tmp_buf).len();
+        for w in tmp_buf {
+            buf.extend(w.to_le_bytes());
+        }
+        Ok(res * 2)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (2, Some(4))
     }
 }
